44f5e4b7919658491dd0db42907c1296f6c9e92e
Avishka-Rathnavibushana <mailavishka@gmail.com>
2023-01-12 08:28:59 +0530

    Add simple changes

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,10 @@ import 'package:pheonix_chat_app/screens/screens.dart';
 
 void main() {
   final client = StreamChatClient(streamKey);
-
-  runApp(
+runApp(
     MyApp(
       client: client,
+      appTheme: AppTheme(),
     ),
   );
 }
@@ -18,16 +18,18 @@ class MyApp extends StatelessWidget {
   const MyApp({
     Key? key,
     required this.client,
+    required this.appTheme,
   }) : super(key: key);
 
   final StreamChatClient client;
+  final AppTheme appTheme;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: appTheme.light,
+      darkTheme: appTheme.dark,
       debugShowCheckedModeBanner: false,
-      theme: AppTheme.light(),
-      darkTheme: AppTheme.dark(),
       themeMode: ThemeMode.dark,
       title: 'Chatter',
       builder: (context, child) {
